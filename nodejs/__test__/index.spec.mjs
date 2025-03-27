c174cc29ca2a730899015f9b16bf0862bcddbaf7
Félix Saparelli <felix@passcod.name>
2025-03-28 11:29:42 +1300

    nodejs: fix test

--- nodejs/__test__/index.spec.mjs
+++ nodejs/__test__/index.spec.mjs
@@ -3,7 +3,7 @@ import test from 'ava'
 import { EndpointKey } from '../index.js'
 
 test('generate and round trip a key', (t) => {
-  const pem1 = EndpointKey.generate().toString()
-  const pem2 = (new EndpointKey(pem1)).toString()
+  const pem1 = EndpointKey.generate().privateKeyPem()
+  const pem2 = (new EndpointKey(pem1)).privateKeyPem()
   t.is(pem1, pem2)
 })
