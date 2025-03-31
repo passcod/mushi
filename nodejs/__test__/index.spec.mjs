f5144032e095cfad4041c1b2b043110da0b53c20
Félix Saparelli <felix@passcod.name>
2025-04-01 01:56:23 +1300

    nodejs: fix test

--- nodejs/__test__/index.spec.mjs
+++ nodejs/__test__/index.spec.mjs
@@ -1,9 +1,9 @@
-import test from 'ava'
+import test from "ava";
 
-import { EndpointKey } from '../index.js'
+import { EndpointKey } from "../index.js";
 
-test('generate and round trip a key', (t) => {
-  const pem1 = EndpointKey.generate().privateKeyPem()
-  const pem2 = (new EndpointKey(pem1)).privateKeyPem()
-  t.is(pem1, pem2)
-})
+test("generate and round trip a key", (t) => {
+  const pem1 = EndpointKey.generate().privateKeyPem;
+  const pem2 = new EndpointKey(pem1).privateKeyPem;
+  t.is(pem1, pem2);
+});
